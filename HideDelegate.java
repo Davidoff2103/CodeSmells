15cf3c4d461da959a63ada0b5767ec3cef97ac81
David Blanco Bautista <73123058+Davidoff2103@users.noreply.github.com>
2025-07-17 12:28:44 +0200

    Rollback

--- HideDelegate.java
+++ HideDelegate.java
@@ -1,166 +1,88 @@
-class City {
-    /**
-     * Represents a city with a name.
-     *
-     * @param name The name of the city.
-     */
+class City2 {
     private String name;
 
-    /**
-     * Constructs a new City instance with the specified name.
-     *
-     * @param name The name of the city.
-     */
-    public City(String name) {
+    public City2(String name) {
         this.name = name;
     }
 
-    /**
-     * Retrieves the name of the city.
-     *
-     * @return The name of the city.
-     */
     public String getName() {
         return name;
     }
 }
 
-class Address {
-    /**
-     * Represents an address with a city and street.
-     *
-     * @param city The city associated with this address.
-     * @param street The street of the address.
-     */
-    private City city;
+class Address2 {
+    private City2 city;
     private String street;
 
-    /**
-     * Constructs a new Address instance with the specified city and street.
-     *
-     * @param city The city associated with this address.
-     * @param street The street of the address.
-     */
-    public Address(City city, String street) {
+    public Address2(City2 city, String street) {
         this.city = city;
         this.street = street;
     }
 
-    /**
-     * Retrieves the city associated with this address.
-     *
-     * @return The city object.
-     */
-    public City getCity() {
+    public City2 getCity() {
         return city;
     }
 
-    /**
-     * Retrieves the street of the address.
-     *
-     * @return The street name.
-     */
     public String getStreet() {
         return street;
     }
 }
 
-class Customer {
-    /**
-     * Represents a customer with an address and name.
-     *
-     * @param address The address of the customer.
-     * @param name The name of the customer.
-     */
-    private Address address;
+class Customer2 {
+    private Address2 address;
     private String name;
 
-    /**
-     * Constructs a new Customer instance with the specified address and name.
-     *
-     * @param address The address of the customer.
-     * @param name The name of the customer.
-     */
-    public Customer(Address address, String name) {
+    public Customer2(Address2 address, String name) {
         this.address = address;
         this.name = name;
     }
 
-    /**
-     * Retrieves the address of the customer.
-     *
-     * @return The address object.
-     */
-    public Address getAddress() {
+    public Address2 getAddress() {
         return address;
     }
 
-    /**
-     * Retrieves the name of the customer.
-     *
-     * @return The customer's name.
-     */
     public String getCustomerName() {
         return name;
     }
 }
 
-class Order {
-    /**
-     * Represents an order with a customer and an ID.
-     *
-     * @param id The unique identifier of the order.
-     * @param customer The customer associated with this order.
-     */
-    private Customer customer;
+class Order2 {
+    private Customer2 customer;
     private int id;
 
-    /**
-     * Constructs a new Order instance with the specified ID and customer.
-     *
-     * @param id The unique identifier of the order.
-     * @param customer The customer associated with this order.
-     */
-    public Order(int id, Customer customer) {
+    public Order2(int id, Customer2 customer) {
         this.id = id;
         this.customer = customer;
     }
 
-    /**
-     * Retrieves the ID of the order.
-     *
-     * @return The order's ID.
-     */
     public int getId() {
         return id;
     }
 
-    /**
-     * Retrieves the customer associated with this order.
-     *
-     * @return The customer object.
-     */
-    public Customer getCustomer() {
-        return customer;
+    // Aplicació de Hide Delegate per obtenir el nom de la ciutat directament
+    public String getCustomerCityName() {
+        return customer.getAddress().getCity().getName();
+    }
+
+    // Aplicació de Hide Delegate per obtenir l'adreça completa (carrer i ciutat)
+    public String getCustomerCompleteAddress() {
+        return customer.getAddress().getStreet() + ", " +
+                customer.getAddress().getCity().getName();
     }
 }
 
-/**
- * Demonstrates the Hide Delegate design pattern by encapsulating access to city information through methods.
- */
 public class HideDelegate {
     public static void main(String[] args) {
-        // Creating instances of objects
-        City city = new City("Lleida");
-        Address address = new Address(city, "c/Jaume II");
-        Customer customer = new Customer(address, "David");
-        Order order = new Order(1, customer);
+        // Creació d'instàncies dels objectes
+        City2 city = new City2("Lleida");
+        Address2 address = new Address2(city, "c/Jaume II");
+        Customer2 customer = new Customer2(address, "David");
+        Order2 order = new Order2(1, customer);
 
-        // Access to the city through encapsulated method
-        System.out.println("Ciutat del client: " + order.getCustomer().getAddress().getCity().getName());
+        // Accés a la ciutat a través del mètode encapsulat
+        System.out.println("Ciutat del client: " + order.getCustomerCityName());
 
-        // Access to the complete address through encapsulated method
-        System.out.println("Adreça completa del client: " + order.getCustomer().getAddress().getStreet() + ", " +
-                order.getCustomer().getAddress().getCity().getName());
+        // Accés a l'adreça completa a través del mètode encapsulat
+        System.out.println("Adreça completa del client: " + order.getCustomerCompleteAddress());
     }
 }
--- MessageChains.java
+++ MessageChains.java
@@ -1,153 +1,68 @@
 class City {
-    /**
-     * Represents a city with a name.
-     *
-     * @param name The name of the city.
-     */
     private String name;
 
-    /**
-     * Constructs a new City instance with the specified name.
-     *
-     * @param name The name of the city.
-     */
     public City(String name) {
         this.name = name;
     }
 
-    /**
-     * Retrieves the name of the city.
-     *
-     * @return The name of the city.
-     */
     public String getName() {
         return name;
     }
 }
 
 class Address {
-    /**
-     * Represents an address with a city and street.
-     *
-     * @param city The city associated with this address.
-     * @param street The street of the address.
-     */
     private City city;
     private String street;
 
-    /**
-     * Constructs a new Address instance with the specified city and street.
-     *
-     * @param city The city associated with this address.
-     * @param street The street of the address.
-     */
     public Address(City city, String street) {
         this.city = city;
         this.street = street;
     }
 
-    /**
-     * Retrieves the city associated with this address.
-     *
-     * @return The city object.
-     */
     public City getCity() {
         return city;
     }
 
-    /**
-     * Retrieves the street of the address.
-     *
-     * @return The street name.
-     */
     public String getStreet() {
         return street;
     }
 }
 
 class Customer {
-    /**
-     * Represents a customer with an address and name.
-     *
-     * @param address The address of the customer.
-     * @param name The name of the customer.
-     */
     private Address address;
     private String name;
 
-    /**
-     * Constructs a new Customer instance with the specified address and name.
-     *
-     * @param address The address of the customer.
-     * @param name The name of the customer.
-     */
     public Customer(Address address, String name) {
         this.address = address;
         this.name = name;
     }
 
-    /**
-     * Retrieves the address of the customer.
-     *
-     * @return The address object.
-     */
     public Address getAddress() {
         return address;
     }
 
-    /**
-     4. Retrieves the name of the customer.
-     *
-     * @return The customer's name.
-     */
     public String getCustomerName() {
         return name;
     }
 }
 
 class Order {
-    /**
-     * Represents an order with a customer and an ID.
-     *
-     * @param id The unique identifier of the order.
-     * @param customer The customer associated with this order.
-     */
     private Customer customer;
     private int id;
 
-    /**
-     * Constructs a new Order instance with the specified ID and customer.
-     *
-     * @param id The unique identifier of the order.
-     * @param customer The customer associated with this order.
-     */
     public Order(int id, Customer customer) {
         this.id = id;
         this.customer = customer;
     }
 
-    /**
-     * Retrieves the ID of the order.
-     *
-     * @return The order's ID.
-     */
     public int getId() {
         return id;
     }
-
-    /**
-     * Retrieves the customer associated with this order.
-     *
-     * @return The customer object.
-     */
     public Customer getCustomer() {
         return customer;
     }
 }
 
-/**
- * Demonstrates the Hide Delegate design pattern by encapsulating access to city information through methods.
- */
 public class MessageChains {
     public static void main(String[] args) {
         // Creació d'instàncies dels objectes
@@ -162,5 +77,6 @@ public class MessageChains {
         System.out.println("Adreça completa del client: " +
                 order.getCustomer().getAddress().getStreet() + ", " +
                 order.getCustomer().getAddress().getCity().getName());
+
     }
 }
